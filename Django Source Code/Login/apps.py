9ac91a76b31ddfd1c74cdc5bade7940308becd15
Scott <liysy020@gmail.com>
2025-07-03 21:28:15 +1000

    Add files via upload
    updated trust IP to CSRF_TRUSTED_ORIGINS = [] in apps.py

--- Django Source Code/Login/apps.py
+++ Django Source Code/Login/apps.py
@@ -9,7 +9,10 @@ class LoginConfig(AppConfig):
     def ready(self):
         import netifaces
         settings.ALLOWED_HOSTS=[]
+        settings.CSRF_TRUSTED_ORIGINS = []
         for interface in netifaces.interfaces():
             interface_info = netifaces.ifaddresses(interface)
             if interface_info.get (netifaces.AF_INET) != None:
-                settings.ALLOWED_HOSTS.append(interface_info.get(netifaces.AF_INET)[0]['addr'])
+                ip = interface_info.get(netifaces.AF_INET)[0]['addr']
+                settings.ALLOWED_HOSTS.append(ip)
+                settings.CSRF_TRUSTED_ORIGINS.append('https://'+ip)
